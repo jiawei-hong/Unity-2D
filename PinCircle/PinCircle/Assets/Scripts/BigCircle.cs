b41cb56c542bb89b821b72c42451689eaba50b8b
zxc455212 <zxc37984@gmail.com>
2019-03-24 18:34:29 +0800

    0324 Unity 2D

---
+++ PinCircle/PinCircle/Assets/Scripts/BigCircle.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class BigCircle : MonoBehaviour {
+    public float Speed = 1;
+
+	void Update () {
+        transform.Rotate(new Vector3(0,0,Speed));
+	}
+}
---
+++ PinCircle/PinCircle/Assets/Scripts/Neddle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Neddle : MonoBehaviour {
+    public float Speed = 1;
+    public bool isFired;
+
+	void Update () {
+        if (Input.GetMouseButtonDown(0)){
+            isFired = true;
+        }
+
+        if (isFired) {
+            transform.Translate(new Vector3(0, Speed, 0));
+        }
+	}
+}
