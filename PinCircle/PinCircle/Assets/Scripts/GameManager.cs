523dc49b3096a9d2ed996a8ec6a6f5dc208e619c
zxc455212 <zxc37984@gmail.com>
2019-03-24 23:28:31 +0800

    做完附著到大圓圈，寫完CreatePin()

---
+++ PinCircle/PinCircle/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public Neddle neddle;
+    public GameObject pinPrefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //CreatePin();
+            neddle.isFired = true;
+        }
+	}
+
+    void CreatePin()
+    {
+        Instantiate(pinPrefab, new Vector3(0, 0, 0), new Quaternion(0,0,0,0));
+    }
+}
--- PinCircle/PinCircle/Assets/Scripts/Neddle.cs
+++ PinCircle/PinCircle/Assets/Scripts/Neddle.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
 
 public class Neddle : MonoBehaviour {
-    public float Speed = 1;
-    public bool isFired;
+    public float Speed = 0.1f;
+    public bool isFired = false;
+    public GameObject Rectangle = null;
 
-	void Update () {
-        if (Input.GetMouseButtonDown(0)){
-            isFired = true;
-        }
-
-        if (isFired) {
+	void Update () {    
+        if (isFired)
+        {
             transform.Translate(new Vector3(0, Speed, 0));
+            Rectangle.SetActive(true);
         }
-	}
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isFired = false;
+        transform.SetParent(collision.transform);
+    }
 }
